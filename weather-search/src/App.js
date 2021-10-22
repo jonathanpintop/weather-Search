67c6876e4905ac92ec6fe49a5ba286bfc56134fc
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-10-22 07:10:36 -0500

    feat: Header added

--- weather-search/src/App.js
+++ weather-search/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment} from "react";
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+<Header
+
+  title='Weather React app'
+/>
+
+<div className="contenedor-form"> 
+<div className="container">
+
+<div className="col m6 s12">1</div>
+<div className="col m6 s12">2</div>
+
+
+
+
+  </div>
+</div>
+
+
+
+    </Fragment>
   );
 }
 
