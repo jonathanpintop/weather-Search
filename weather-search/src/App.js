3f4034eea22440cb4826347df2f596a0294bbbb9
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-10-25 16:43:51 -0500

    feat: Prinring on html feature added

--- weather-search/src/App.js
+++ weather-search/src/App.js
@@ -1,7 +1,73 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useState,useEffect} from "react";
 import Header from './components/Header';
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import Error from "./components/Error";
+
+
+
 
 function App() {
+
+  // state del formulario
+  const [search, setSearch] = useState({
+    city: "",
+    country: "",
+  });
+
+const [fetch,setFetch]= useState(false);
+const [result,setResult] = useState({});
+const [error,setError]= useState(false);
+
+  const { city, country } = search;
+
+
+useEffect(()=> {
+const fetchApi = async () => {
+
+if(fetchApi) {
+
+  const appId = 'a57ff30b3cfbc3b1e86ffa3ec24330b1';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
+
+const response = await fetch(url);
+const result = await response.json();
+
+
+setResult(result);
+setFetch(false);
+}
+
+// Detecting if there fetch was right
+
+if(result.cod === "404") {
+  setError(true);
+}else{
+  setError(false);
+
+}
+
+
+}
+
+fetchApi()
+
+},[fetch]);
+
+let component;
+
+
+if(error){
+ component = <Error message="There is not results" />
+}else {
+  component = <Weather
+    result={result}
+
+  />
+}
+
+
+
   return (
     <Fragment>
 <Header
@@ -11,19 +77,29 @@ function App() {
 
 <div className="contenedor-form"> 
 <div className="container">
-
-<div className="col m6 s12">1</div>
-<div className="col m6 s12">2</div>
-
-
-
-
-  </div>
+<div className="row">
+<div className="col m6 s12">
+  <Form
+  search={search}
+  setSearch={setSearch}
+  fetch={setFetch}
+  
+  
+  />
+</div>
+<div className="col m6 s12">
+  {component}
 </div>
 
 
 
+</div>
+  </div>
+</div>
+
+
     </Fragment>
+    
   );
 }
 
